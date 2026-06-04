eab9b3215ece333929a0fa1cbff57e62a3e9936f
seopyo <seopyo26@gmail.com>
2026-06-04 15:36:53 +0900

    #27
    feat: Components/Transform

---
+++ Princess_Cannot_Walk_Unpaved_Road/Princess_Cannot_Walk_Unpaved_Road/Source/Engine/Components/Transform.cpp
@@ -0,0 +1,21 @@
+#include "Transform.h"
+#include "Engine/Object/GameObject.h"
+
+namespace Bisang
+{
+    Vector2 Transform::GetWorldPosition()
+    {
+        // 부모가 없으면 현재 위치가 월드 위치
+        if (nullptr == m_ownerObj->GetParent())
+        {
+            return m_pos;
+        }
+
+        // 부모의 월드 위치에 자신의 로컬 위치를 더해서 월드 위치를 계산
+        return m_ownerObj
+            ->GetParent()
+            ->GetComponent<Transform>()
+            ->GetWorldPosition()
+            + m_pos;
+    }
+}
